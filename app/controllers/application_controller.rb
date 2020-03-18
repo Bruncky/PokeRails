a98a5a05fee64e2becc85a5d47bab8c41022c1ce
Bruncky <brunobrunckhorst@gmail.com>
2020-03-18 02:02:15 +0100

    Overriding default_domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
+
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+end
 end
