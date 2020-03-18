577939a7e8824e23ab0229859c71723143bfa9d9
Bruncky <brunobrunckhorst@gmail.com>
2020-03-18 14:50:14 +0100

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
     devise_for :users
 
     resources :users, only: %i[show]
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
