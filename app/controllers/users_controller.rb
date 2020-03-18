a07d279c8b44d6764f75bb9ab84d6ac360dcb6b6
Bruncky <brunobrunckhorst@gmail.com>
2020-03-18 03:13:53 +0100

    Added new to users#controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def show
       @user = current_user
   end
+
+  def new
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :encrypted_password)
+  end
 end
