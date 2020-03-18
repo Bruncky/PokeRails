c770ff8d9246c397700e3feb90e578fd3ec9c705
Bruncky <brunobrunckhorst@gmail.com>
2020-03-18 02:16:16 +0100

    Fixed routes for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root to: 'pages#home'
+
+    devise_for :users
+
+    resources :users, only: %i[show]
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
