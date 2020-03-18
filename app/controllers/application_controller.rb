21902b5c985f83f5a5cb61fee24df31a7034772e
Bruncky <brunobrunckhorst@gmail.com>
2020-03-18 02:05:27 +0100

    Indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+    protected
 
-  def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
-  end
+    def configure_permitted_parameters
+        # devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    end
 
-  def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
-end
+    def default_url_options
+        { host: ENV["DOMAIN"] || "localhost:3000" }
+    end
 end
