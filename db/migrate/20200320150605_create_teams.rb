6ad7e78620e3e86f0ce52eb686152cf276f5c522
Bruncky <brunobrunckhorst@gmail.com>
2020-03-20 16:16:45 +0100

    Removed manually generated Migration

--- db/migrate/20200320150605_create_teams.rb
+++
@@ -1,9 +0,0 @@
-class CreateTeams < ActiveRecord::Migration[5.2]
-  def change
-    create_table :teams do |t|
-      t.string :name
-      t.string :pokemon, array: true, default: []
-      t.references :user, foreign_key: true
-    end
-  end
-end
