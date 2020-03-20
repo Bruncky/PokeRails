008dcd4d5d2e4da0b794094502f0f133772f1a54
Bruncky <brunobrunckhorst@gmail.com>
2020-03-20 16:07:38 +0100

    Make the 'pokemon' column in Teams into an Array

--- db/migrate/20200320150605_create_teams.rb
+++ db/migrate/20200320150605_create_teams.rb
@@ -2,7 +2,7 @@ class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
       t.string :name
-      t.string :pokemon
+      t.string :pokemon, array: true, default: []
       t.references :user, foreign_key: true
     end
   end
