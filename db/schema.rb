3673dd34342185b88b28267bf562436104e49904
Bruncky <brunobrunckhorst@gmail.com>
2020-03-20 16:24:07 +0100

    Migrated DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_150605) do
+ActiveRecord::Schema.define(version: 2020_03_20_151735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_03_20_150605) do
     t.string "name"
     t.string "pokemon", default: [], array: true
     t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
