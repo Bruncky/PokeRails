98f3b7d8f10abc00c3ad1417a5fb35db81557ff1
Bruncky <brunobrunckhorst@gmail.com>
2020-03-20 15:58:14 +0100

    Added CRUD and team_params to TeamsController (skelletons)

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,27 @@
 class TeamsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+    def team_params
+    end
 end
