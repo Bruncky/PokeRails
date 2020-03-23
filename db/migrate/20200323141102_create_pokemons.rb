69a69ce46a2a57904c3cbeeed34b8c96404d6944
Bruncky <brunobrunckhorst@gmail.com>
2020-03-23 15:11:18 +0100

    Generated the Pokémon model

---
+++ db/migrate/20200323141102_create_pokemons.rb
@@ -0,0 +1,23 @@
+class CreatePokemons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pokemons do |t|
+      t.string :generation
+      t.string :region
+      t.integer :number
+      t.string :name
+      t.string :nickname
+      t.string :gender
+      t.string :type
+      t.string :evolutions
+      t.string :nature
+      t.string :ability
+      t.string :moves
+      t.string :learnable_moves
+      t.string :egg
+      t.string :held_item
+      t.string :stats
+
+      t.timestamps
+    end
+  end
+end
