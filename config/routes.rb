5c722925b9d5c97d9fc995c38f87591879b59d2c
Bruncky <brunobrunckhorst@gmail.com>
2020-03-23 17:04:34 +0100

    Added routes for Pokémon Model Nested in Users and nested in Teams. This is to reflect the fact that a User will have a list of all the Pokémon they have captured (belongs_to user) and Teams will also have a list of all Pokémon in that team (belongs_to team)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     devise_for :users
 
     resources :users, only: %i[show] do
-        resources :teams
+        resources :pokemons
+        
+        resources :teams do
+            resources :pokemons
+        end
     end
 end
