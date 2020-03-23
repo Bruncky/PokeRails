47e98b1fb4eb2e009b8f34db401ab173f3cb9a55
Bruncky <brunobrunckhorst@gmail.com>
2020-03-23 16:54:25 +0100

    Added Array values to migration

--- db/migrate/20200323154852_create_pokemons.rb
+++ db/migrate/20200323154852_create_pokemons.rb
@@ -3,7 +3,7 @@ class CreatePokemons < ActiveRecord::Migration[5.2]
     create_table :pokemons do |t|
       t.string :generation
       t.string :pokedex_region
-      t.string :locations
+      t.string :locations, array: true, default: []
       t.integer :number
       t.string :name
       t.string :nickname
@@ -12,8 +12,8 @@ class CreatePokemons < ActiveRecord::Migration[5.2]
       t.json :evolutions
       t.string :nature
       t.string :ability
-      t.string :moves
-      t.string :learnable_moves
+      t.string :moves, array: true, default: []
+      t.string :learnable_moves, array: true, default: []
       t.json :egg
       t.string :held_item
       t.json :stats
