9e322560bd7b3c917699a685bb2ee27d7a0075e5
Bruncky <brunobrunckhorst@gmail.com>
2020-03-23 16:49:44 +0100

    Changed Migration to reflect JSON (Hash) datatype necessity

--- db/migrate/20200323154852_create_pokemons.rb
+++ db/migrate/20200323154852_create_pokemons.rb
@@ -7,16 +7,16 @@ class CreatePokemons < ActiveRecord::Migration[5.2]
       t.integer :number
       t.string :name
       t.string :nickname
-      t.string :gender
+      t.json :gender
       t.string :type
-      t.string :evolutions
+      t.json :evolutions
       t.string :nature
       t.string :ability
       t.string :moves
       t.string :learnable_moves
-      t.string :egg
+      t.json :egg
       t.string :held_item
-      t.string :stats
+      t.json :stats
       t.references :user, foreign_key: true
       t.references :team, foreign_key: true
 
