87d15b13b170c2dbcd3fd579c34885ef9197bf43
Bruncky <brunobrunckhorst@gmail.com>
2020-03-24 15:29:31 +0100

    Added index and show routes to Pokémons

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
     devise_for :users
 
+    resources :pokemons, only: %i[index show]
+
     resources :users, only: %i[show] do
         resources :pokemons
-        
+
         resources :teams do
             resources :pokemons
         end
