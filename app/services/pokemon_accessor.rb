b03c6802eea789140f1cd82c0c92e918a926e010
Bruncky <brunobrunckhorst@gmail.com>
2020-03-26 17:52:49 +0100

    Added scraper for Generation and Region of each Pokémon

--- app/services/pokemon_accessor.rb
+++ app/services/pokemon_accessor.rb
@@ -2,7 +2,28 @@ require 'open-uri'
 require 'nokogiri'
 
 class PokemonAccessor
+    def initialize
+        @regions = {
+            "Generation 1" => "Kanto",
+            "Generation 2" => "Johto",
+            "Generation 3" => "Hoenn",
+            "Generation 4" => "Sinnoh",
+            "Generation 5" => "Unova",
+            "Generation 6" => "Kalos",
+            "Generation 7" => "Alola",
+            "Generation 8" => "Galar"
+        }
+    end
+
     def call
+        scrape_names[0..8].each do |pokemon|
+            save_pokemon(pokemon)
+        end
+    end
+
+    # private
+
+    def scrape_names
         # Opens the URL and reads it
         html_doc = Nokogiri::HTML(open("https://pokemondb.net/pokedex/national").read)
 
@@ -11,4 +32,147 @@ class PokemonAccessor
             element.text
         end
     end
+
+    def save_pokemon(pokemon)
+        pokemon = Pokemon.new(
+            generation: scrape_generation(pokemon),
+            pokedex_region: @regions[scrape_generation(pokemon)],
+            locations: {
+                "sword":
+                [
+                    {
+                        "max raid battles":
+                        [
+                            "Giant's Cap",
+                            "Motostoke Riverbank"
+                        ]
+                    }
+                ],
+                "shield":
+                [
+                    {
+                        "max raid battles":
+                        [
+                            "Giant's Cap",
+                            "Motostoke Riverbank"
+                        ]
+                    }
+                ]
+            },
+            number: 26,
+            name: pokemon,
+            gender: {
+                "gender": "Male",
+                "ratio":
+                {
+                    "male": 50.2,
+                    "female": 49.8
+                }
+            },
+            types: ["Electric"],
+            level: 100,
+            nature: "Bashful",
+            ability: {
+                "name": "Static",
+                "description": "The opponent has a 30% chance of being induced with PARALYZE when using an attack, that requires physical contact, against this Pokémon."
+            },
+            moves: {
+                "Thunderbolt": {
+                    "type": "Electric",
+                    "category": "Special",
+                    "attack": 90,
+                    "accuracy": 100,
+                    "PP": 15,
+                    "effect %": 10,
+                    "description": "A strong electric blast crashes down on the target. This may also leave the target with paralysis."
+                },
+                "Thunder": {
+                    "type": "Electric",
+                    "category": "Effect",
+                    "attack": 110,
+                    "accuracy": 70,
+                    "PP": 10,
+                    "effect %": 30,
+                    "description": "A wicked thunderbolt is dropped on the target to inflict damage. This may also leave the target with paralysis."
+                },
+                "Electro Ball": {
+                    "type": "Electric",
+                    "category": "Special",
+                    "accuracy": 100,
+                    "PP": 10,
+                    "effect %": 0,
+                    "description": "The user hurls an electric orb at the target. The faster the user is than the target, the greater the move's power."
+                },
+                "Nuzzle": {
+                    "type": "Electric",
+                    "category": "Physical",
+                    "attack": 20,
+                    "accuracy": 100,
+                    "PP": 20,
+                    "effect %": 100,
+                    "description": "The user attacks by nuzzling its electrified cheeks against the target. This also leaves the target with paralysis."
+                }
+            },
+            learnable_moves: {
+                "Discharge": {
+                    "level": 32,
+                    "type": "Electric",
+                    "category": "Special",
+                    "attack": 80,
+                    "accuracy": 100,
+                    "PP": 15,
+                    "effect %": 30,
+                    "description": "The user strikes everything around it by letting loose a flare of electricity. This may also cause paralysis."
+                }
+            },
+            held_item: "Magnet",
+            stats: {
+                "HP": 267,
+                "Attack": 250,
+                "Defense": 136,
+                "Speed": 274,
+                "Sp. Def": 180,
+                "Sp. Attack": 223
+            }
+        )
+        # -----
+        pokemon.user = User.last
+        pokemon.team = Team.first
+        # -----
+        # pokemon_photo = URI.open("https://res.cloudinary.com/bruncky/image/upload/v1585056141/Pok%C3%A9Rails/Seed%20Images/Raichu.png")
+
+        # pokemon.photos.attach(io: pokemon_photo, filename: 'pokemon.png', content_type: 'image/png')
+        # -----
+        pokemon.save!
+    end
+
+    def scrape_generation(pokemon)
+        # Opens the URL and reads it
+        html_doc = Nokogiri::HTML(open("https://pokemondb.net/pokedex/#{pokemon}").read)
+
+        # Searches the URL for the given CSS selector
+        html_doc.search('.data-table.sprites-table.sprites-history-table > thead > tr th[2]').text
+    end
+
+    # def scrape_test
+    #     # Opens the URL and reads it
+    #     html_doc = Nokogiri::HTML(open("https://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pokemon)").read)
+    #
+    #     games_hash = {}
+    #     # Searches the URL for the given CSS selector
+    #     # This gets all the games + useless stuff
+    #     html_doc.search('.roundytr.roundybottom .roundy th a').map do |game|
+    #         game.text.strip
+    #     end.reject! { |string| string.nil? || string.empty? }.map do |name|
+    #         games_hash[name] = []
+    #     end
+    #
+    #     html_doc.search('.roundytr.roundybottom .roundy td').map do |location|
+    #         location.text.strip
+    #     end.reject! { |string| string.nil? || string.empty? }.each_with_index do |location_name, index|
+    #         games_hash[]  location_name
+    #     end
+    #
+    #     games_hash
+    # end
 end
